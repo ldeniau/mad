09fefb4d68992f0aec71d0af74d89a8edf911e03
martinvalen <martin.valen1@gmail.com>
2014-01-09 17:01:05 +0100

    Fixed some comments.

--- src/lib/lpeg.lua
+++ src/lib/lpeg.lua
@@ -1,3 +1,8 @@
+--[[
+Wrapper for lpeg.so.
+Requires and returns lpeg.so, keeping the cpath as it is.
+]]
+
 local pcp = package.cpath
 package.cpath = ";;./lib/lpeg/lpeg-0.12/?.so;.\\lib\\?\\lpeg-0.12\\?.dll;"
 local lpeg = require"lpeg"
--- src/lib/lpeg/re.lua
+++ src/lib/lpeg/re.lua
@@ -1,3 +1,8 @@
+--[[
+Wrapper for re.lua.
+Requires re.lua keeping the path and cpath as they were before.
+]]
+
 local pcp = package.cpath
 local pp = package.path
 package.cpath = ";;./lib/lpeg/lpeg-0.12/?.so;.\\lib\\lpeg\\lpeg-0.12\\?.dll;"
@@ -5,5 +10,4 @@ package.path = ";;./lib/lpeg/lpeg-0.12/?.lua;.\\lib\\lpeg\\lpeg-0.12\\?.lua;"
 local re =  require"re"
 package.cpath = pcp
 package.path = pp
-
 return re
