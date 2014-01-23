48342e42689e7d4be6733f8649e7a616cf5c07a1
martinvalen <martin.valen1@gmail.com>
2014-01-23 16:44:41 +0100

    Changed the layout of the benchmark

--- src/mad/benchmark/object.lua
+++ src/mad/benchmark/object.lua
@@ -2,35 +2,33 @@ local object = require"mad.object"
 
 print"Benchmarking how fast one can create objects and how much memory they require"
 
-local n = 10000
+local n = 1000
 
 do
     local obj1 = object
     local obj2, start, stop
-    local total = 0
+    local obj = {}
+    start = collectgarbage"count"
     for i = 1, n do
-        start = collectgarbage"count"
-        obj2 = obj1 ""
-        stop = collectgarbage"count"
-        total = total + stop - start
+        obj[#obj+1] = obj1 ""
     end
+    stop = collectgarbage"count"
     print("1st level, name = ''")
-    print("  Average memory used: "..tostring(total*1024/n))
+    print("  Average memory used: "..tostring((stop-start)*1024/n))
 end
 
 do
     local obj1 = object
     local obj2 = obj1 ""
     local obj3, start, stop
-    local total = 0
+    local obj = {}
+    start = collectgarbage"count"
     for i = 1, n do
-        start = collectgarbage"count"
-        obj3 = obj2 ""
-        stop = collectgarbage"count"
-        total = total + stop - start
+        obj[#obj+1] = obj2 ""
     end
+    stop = collectgarbage"count"
     print("2nd level, name = ''")
-    print("  Average memory used: "..tostring(total*1024/n))
+    print("  Average memory used: "..tostring((stop-start)*1024/n))
 end
 
 do
@@ -38,15 +36,14 @@ do
     local obj2 = obj1 ""
     local obj3 = obj2 ""
     local obj4, start, stop
-    local total = 0
+    local obj = {}
+    start = collectgarbage"count"
     for i = 1, n do
-        start = collectgarbage"count"
-        obj4 = obj3 ""
-        stop = collectgarbage"count"
-        total = total + stop - start
+        obj[#obj+1] = obj3 ""
     end
+    stop = collectgarbage"count"
     print("3rd level, name = ''")
-    print("  Average memory used: "..tostring(total*1024/n))
+    print("  Average memory used: "..tostring((stop-start)*1024/n))
 end
 
 ----------------------------------------------------------------------
@@ -54,30 +51,28 @@ end
 do
     local obj1 = object
     local obj2, start, stop
-    local total = 0
+    local obj = {}
+    start = collectgarbage"count"
     for i = 1, n do
-        start = collectgarbage"count"
-        obj2 = obj1 "obj2"
-        stop = collectgarbage"count"
-        total = total + stop - start
+        obj[#obj+1] = obj1 "obj2"
     end
+    stop = collectgarbage"count"
     print("1st level, name = 'obj2'")
-    print("  Average memory used: "..tostring(total*1024/n))
+    print("  Average memory used: "..tostring((stop-start)*1024/n))
 end
 
 do
     local obj1 = object
     local obj2 = obj1 "obj2"
     local obj3, start, stop
-    local total = 0
+    local obj = {}
+    start = collectgarbage"count"
     for i = 1, n do
-        start = collectgarbage"count"
-        obj3 = obj2 "obj3"
-        stop = collectgarbage"count"
-        total = total + stop - start
+        obj[#obj+1] = obj2 "obj3"
     end
+    stop = collectgarbage"count"
     print("2nd level, name = 'obj3'")
-    print("  Average memory used: "..tostring(total*1024/n))
+    print("  Average memory used: "..tostring((stop-start)*1024/n))
 end
 
 do
@@ -85,15 +80,14 @@ do
     local obj2 = obj1 "obj2"
     local obj3 = obj2 "obj3"
     local obj4, start, stop
-    local total = 0
+    local obj = {}
+    start = collectgarbage"count"
     for i = 1, n do
-        start = collectgarbage"count"
-        obj4 = obj3 "obj4"
-        stop = collectgarbage"count"
-        total = total + stop - start
+        obj[#obj+1] = obj3 "obj4"
     end
+    stop = collectgarbage"count"
     print("3rd level, name = 'obj4'")
-    print("  Average memory used: "..tostring(total*1024/n))
+    print("  Average memory used: "..tostring((stop-start)*1024/n))
 end
 
 ----------------------------------------------------------------------
@@ -101,30 +95,28 @@ end
 do
     local obj1 = object { one = 1 }
     local obj2, start, stop
-    local total = 0
+    local obj = {}
+    start = collectgarbage"count"
     for i = 1, n do
-        start = collectgarbage"count"
-        obj2 = obj1 "obj2" { two = 2 }
-        stop = collectgarbage"count"
-        total = total + stop - start
+        obj[#obj+1] = obj1 "obj2" { two = 2 }
     end
+    stop = collectgarbage"count"
     print("1st level, name = 'obj2', 1 object per level")
-    print("  Average memory used: "..tostring(total*1024/n))
+    print("  Average memory used: "..tostring((stop-start)*1024/n))
 end
 
 do
     local obj1 = object { one = 1 }
     local obj2 = obj1 "obj2" { two = 2 }
     local obj3, start, stop
-    local total = 0
+    local obj = {}
+    start = collectgarbage"count"
     for i = 1, n do
-        start = collectgarbage"count"
-        obj3 = obj2 "obj3" { three = 3 }
-        stop = collectgarbage"count"
-        total = total + stop - start
+        obj[#obj+1] = obj2 "obj3" { three = 3 }
     end
+    stop = collectgarbage"count"
     print("2nd level, name = 'obj3', 1 object per level")
-    print("  Average memory used: "..tostring(total*1024/n))
+    print("  Average memory used: "..tostring((stop-start)*1024/n))
 end
 
 do
@@ -132,15 +124,14 @@ do
     local obj2 = obj1 "obj2" { two = 2 }
     local obj3 = obj2 "obj3" { three = 3 }
     local obj4, start, stop
-    local total = 0
+    local obj = {}
+    start = collectgarbage"count"
     for i = 1, n do
-        start = collectgarbage"count"
-        obj4 = obj3 "obj4" { four = 4 }
-        stop = collectgarbage"count"
-        total = total + stop - start
+        obj[#obj+1] = obj3 "obj4" { four = 4 }
     end
+    stop = collectgarbage"count"
     print("3rd level, name = 'obj4', 1 object per level")
-    print("  Average memory used: "..tostring(total*1024/n))
+    print("  Average memory used: "..tostring((stop-start)*1024/n))
 end
 
 ----------------------------------------------------------------------
@@ -148,30 +139,28 @@ end
 do
     local obj1 = object { one = 1, one1 = 1 }
     local obj2, start, stop
-    local total = 0
+    local obj = {}
+    start = collectgarbage"count"
     for i = 1, n do
-        start = collectgarbage"count"
-        obj2 = obj1 "obj2" { two = 2, two2 = 2 }
-        stop = collectgarbage"count"
-        total = total + stop - start
+        obj[#obj+1] = obj1 "obj2" { two = 2, two2 = 2 }
     end
+    stop = collectgarbage"count"
     print("1st level, name = 'obj2', 2 objects per level")
-    print("  Average memory used: "..tostring(total*1024/n))
+    print("  Average memory used: "..tostring((stop-start)*1024/n))
 end
 
 do
     local obj1 = object { one = 1, one1 = 1 }
     local obj2 = obj1 "obj2" { two = 2, two2 = 2 }
     local obj3, start, stop
-    local total = 0
+    local obj = {}
+    start = collectgarbage"count"
     for i = 1, n do
-        start = collectgarbage"count"
-        obj3 = obj2 "obj3" { three = 3, three3 = 3 }
-        stop = collectgarbage"count"
-        total = total + stop - start
+        obj[#obj+1] = obj2 "obj3" { three = 3, three3 = 3 }
     end
+    stop = collectgarbage"count"
     print("2nd level, name = 'obj3', 2 objects per level")
-    print("  Average memory used: "..tostring(total*1024/n))
+    print("  Average memory used: "..tostring((stop-start)*1024/n))
 end
 
 do
@@ -179,15 +168,14 @@ do
     local obj2 = obj1 "obj2" { two = 2, two2 = 2 }
     local obj3 = obj2 "obj3" { three = 3, three3 = 3 }
     local obj4, start, stop
-    local total = 0
+    local obj = {}
+    start = collectgarbage"count"
     for i = 1, n do
-        start = collectgarbage"count"
-        obj4 = obj3 "obj4" { four = 4, four4 = 4 }
-        stop = collectgarbage"count"
-        total = total + stop - start
+        obj[#obj+1] = obj3 "obj4" { four = 4, four4 = 4 }
     end
+    stop = collectgarbage"count"
     print("3rd level, name = 'obj4', 2 objects per level")
-    print("  Average memory used: "..tostring(total*1024/n))
+    print("  Average memory used: "..tostring((stop-start)*1024/n))
 end
 
 ----------------------------------------------------------------------
@@ -195,30 +183,28 @@ end
 do
     local obj1 = object { one = 1, one1 = 1, one11 = 1 }
     local obj2, start, stop
-    local total = 0
+    local obj = {}
+    start = collectgarbage"count"
     for i = 1, n do
-        start = collectgarbage"count"
-        obj2 = obj1 "obj2" { two = 2, two2 = 2, two22 = 2 }
-        stop = collectgarbage"count"
-        total = total + stop - start
+        obj[#obj+1] = obj1 "obj2" { two = 2, two2 = 2, two22 = 2 }
     end
+    stop = collectgarbage"count"
     print("1st level, name = 'obj2', 3 objects per level")
-    print("  Average memory used: "..tostring(total*1024/n))
+    print("  Average memory used: "..tostring((stop-start)*1024/n))
 end
 
 do
     local obj1 = object { one = 1, one1 = 1, one11 = 1 }
     local obj2 = obj1 "obj2" { two = 2, two2 = 2, two22 = 2 }
     local obj3, start, stop
-    local total = 0
+    local obj = {}
+    start = collectgarbage"count"
     for i = 1, n do
-        start = collectgarbage"count"
-        obj3 = obj2 "obj3" { three = 3, three3 = 3, three33 = 3 }
-        stop = collectgarbage"count"
-        total = total + stop - start
+        obj[#obj+1] = obj2 "obj3" { three = 3, three3 = 3, three33 = 3 }
     end
+    stop = collectgarbage"count"
     print("2nd level, name = 'obj3', 3 objects per level")
-    print("  Average memory used: "..tostring(total*1024/n))
+    print("  Average memory used: "..tostring((stop-start)*1024/n))
 end
 
 do
@@ -226,47 +212,44 @@ do
     local obj2 = obj1 "obj2" { two = 2, two2 = 2, two22 = 2 }
     local obj3 = obj2 "obj3" { three = 3, three3 = 3, three33 = 3 }
     local obj4, start, stop
-    local total = 0
+    local obj = {}
+    start = collectgarbage"count"
     for i = 1, n do
-        start = collectgarbage"count"
-        obj4 = obj3 "obj4" { four = 4, four4 = 4, four44 = 4 }
-        stop = collectgarbage"count"
-        total = total + stop - start
+        obj[#obj+1] = obj3 "obj4" { four = 4, four4 = 4, four44 = 4 }
     end
+    stop = collectgarbage"count"
     print("3rd level, name = 'obj4', 3 objects per level")
-    print("  Average memory used: "..tostring(total*1024/n))
+    print("  Average memory used: "..tostring((stop-start)*1024/n))
 end
 
 ----------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------
+n = 10000000
+
 do
     local obj1 = object
     local obj2, start, stop
-    local total = 0
+    start = os.clock()
     for i = 1, n do
-        start = os.clock()
         obj2 = obj1 ""
-        stop = os.clock()
-        total = total + stop - start
     end
+    stop = os.clock()
     print("1st level, name = ''")
-    print("  Average speed: "..tostring(total/n))
+    print("  Average speed: "..tostring((stop-start)*1000000/n).."us")
 end
 
 do
     local obj1 = object
     local obj2 = obj1 ""
     local obj3, start, stop
-    local total = 0
+    start = os.clock()
     for i = 1, n do
-        start = os.clock()
         obj3 = obj2 ""
-        stop = os.clock()
-        total = total + stop - start
     end
+    stop = os.clock()
     print("2nd level, name = ''")
-    print("  Average speed: "..tostring(total/n))
+    print("  Average speed: "..tostring((stop-start)*1000000/n).."us")
 end
 
 do
@@ -274,15 +257,13 @@ do
     local obj2 = obj1 ""
     local obj3 = obj2 ""
     local obj4, start, stop
-    local total = 0
+    start = os.clock()
     for i = 1, n do
-        start = os.clock()
         obj4 = obj3 ""
-        stop = os.clock()
-        total = total + stop - start
     end
+    stop = os.clock()
     print("3rd level, name = ''")
-    print("  Average speed: "..tostring(total/n))
+    print("  Average speed: "..tostring((stop-start)*1000000/n).."us")
 end
 
 ----------------------------------------------------------------------
@@ -290,30 +271,26 @@ end
 do
     local obj1 = object
     local obj2, start, stop
-    local total = 0
+    start = os.clock()
     for i = 1, n do
-        start = os.clock()
         obj2 = obj1 "obj2"
-        stop = os.clock()
-        total = total + stop - start
     end
+    stop = os.clock()
     print("1st level, name = 'obj2'")
-    print("  Average speed: "..tostring(total/n))
+    print("  Average speed: "..tostring((stop-start)*1000000/n).."us")
 end
 
 do
     local obj1 = object
     local obj2 = obj1 "obj2"
     local obj3, start, stop
-    local total = 0
+    start = os.clock()
     for i = 1, n do
-        start = os.clock()
         obj3 = obj2 "obj3"
-        stop = os.clock()
-        total = total + stop - start
     end
+    stop = os.clock()
     print("2nd level, name = 'obj3'")
-    print("  Average speed: "..tostring(total/n))
+    print("  Average speed: "..tostring((stop-start)*1000000/n).."us")
 end
 
 do
@@ -321,15 +298,13 @@ do
     local obj2 = obj1 "obj2"
     local obj3 = obj2 "obj3"
     local obj4, start, stop
-    local total = 0
+    start = os.clock()
     for i = 1, n do
-        start = os.clock()
         obj4 = obj3 "obj4"
-        stop = os.clock()
-        total = total + stop - start
     end
+    stop = os.clock()
     print("3rd level, name = 'obj4'")
-    print("  Average speed: "..tostring(total/n))
+    print("  Average speed: "..tostring((stop-start)*1000000/n).."us")
 end
 
 ----------------------------------------------------------------------
@@ -337,30 +312,26 @@ end
 do
     local obj1 = object { one = 1 }
     local obj2, start, stop
-    local total = 0
+    start = os.clock()
     for i = 1, n do
-        start = os.clock()
         obj2 = obj1 "obj2" { two = 2 }
-        stop = os.clock()
-        total = total + stop - start
     end
+    stop = os.clock()
     print("1st level, name = 'obj2', 1 object per level")
-    print("  Average speed: "..tostring(total/n))
+    print("  Average speed: "..tostring((stop-start)*1000000/n).."us")
 end
 
 do
     local obj1 = object { one = 1 }
     local obj2 = obj1 "obj2" { two = 2 }
     local obj3, start, stop
-    local total = 0
+    start = os.clock()
     for i = 1, n do
-        start = os.clock()
         obj3 = obj2 "obj3" { three = 3 }
-        stop = os.clock()
-        total = total + stop - start
     end
+    stop = os.clock()
     print("2nd level, name = 'obj3', 1 object per level")
-    print("  Average speed: "..tostring(total/n))
+    print("  Average speed: "..tostring((stop-start)*1000000/n).."us")
 end
 
 do
@@ -368,15 +339,13 @@ do
     local obj2 = obj1 "obj2" { two = 2 }
     local obj3 = obj2 "obj3" { three = 3 }
     local obj4, start, stop
-    local total = 0
+    start = os.clock()
     for i = 1, n do
-        start = os.clock()
         obj4 = obj3 "obj4" { four = 4 }
-        stop = os.clock()
-        total = total + stop - start
     end
+    stop = os.clock()
     print("3rd level, name = 'obj4', 1 object per level")
-    print("  Average speed: "..tostring(total/n))
+    print("  Average speed: "..tostring((stop-start)*1000000/n).."us")
 end
 
 ----------------------------------------------------------------------
@@ -384,30 +353,26 @@ end
 do
     local obj1 = object { one = 1, one1 = 1 }
     local obj2, start, stop
-    local total = 0
+    start = os.clock()
     for i = 1, n do
-        start = os.clock()
         obj2 = obj1 "obj2" { two = 2, two2 = 2 }
-        stop = os.clock()
-        total = total + stop - start
     end
+    stop = os.clock()
     print("1st level, name = 'obj2', 2 objects per level")
-    print("  Average speed: "..tostring(total/n))
+    print("  Average speed: "..tostring((stop-start)*1000000/n).."us")
 end
 
 do
     local obj1 = object { one = 1, one1 = 1 }
     local obj2 = obj1 "obj2" { two = 2, two2 = 2 }
     local obj3, start, stop
-    local total = 0
+    start = os.clock()
     for i = 1, n do
-        start = os.clock()
         obj3 = obj2 "obj3" { three = 3, three3 = 3 }
-        stop = os.clock()
-        total = total + stop - start
     end
+    stop = os.clock()
     print("2nd level, name = 'obj3', 2 objects per level")
-    print("  Average speed: "..tostring(total/n))
+    print("  Average speed: "..tostring((stop-start)*1000000/n).."us")
 end
 
 do
@@ -415,15 +380,13 @@ do
     local obj2 = obj1 "obj2" { two = 2, two2 = 2 }
     local obj3 = obj2 "obj3" { three = 3, three3 = 3 }
     local obj4, start, stop
-    local total = 0
+    start = os.clock()
     for i = 1, n do
-        start = os.clock()
         obj4 = obj3 "obj4" { four = 4, four4 = 4 }
-        stop = os.clock()
-        total = total + stop - start
     end
+    stop = os.clock()
     print("3rd level, name = 'obj4', 2 objects per level")
-    print("  Average speed: "..tostring(total/n))
+    print("  Average speed: "..tostring((stop-start)*1000000/n).."us")
 end
 
 ----------------------------------------------------------------------
@@ -431,30 +394,26 @@ end
 do
     local obj1 = object { one = 1, one1 = 1, one11 = 1 }
     local obj2, start, stop
-    local total = 0
+    start = os.clock()
     for i = 1, n do
-        start = os.clock()
         obj2 = obj1 "obj2" { two = 2, two2 = 2, two22 = 2 }
-        stop = os.clock()
-        total = total + stop - start
     end
+    stop = os.clock()
     print("1st level, name = 'obj2', 3 objects per level")
-    print("  Average speed: "..tostring(total/n))
+    print("  Average speed: "..tostring((stop-start)*1000000/n).."us")
 end
 
 do
     local obj1 = object { one = 1, one1 = 1, one11 = 1 }
     local obj2 = obj1 "obj2" { two = 2, two2 = 2, two22 = 2 }
     local obj3, start, stop
-    local total = 0
+    start = os.clock()
     for i = 1, n do
-        start = os.clock()
         obj3 = obj2 "obj3" { three = 3, three3 = 3, three33 = 3 }
-        stop = os.clock()
-        total = total + stop - start
     end
+    stop = os.clock()
     print("2nd level, name = 'obj3', 3 objects per level")
-    print("  Average speed: "..tostring(total/n))
+    print("  Average speed: "..tostring((stop-start)*1000000/n).."us")
 end
 
 do
@@ -462,15 +421,13 @@ do
     local obj2 = obj1 "obj2" { two = 2, two2 = 2, two22 = 2 }
     local obj3 = obj2 "obj3" { three = 3, three3 = 3, three33 = 3 }
     local obj4, start, stop
-    local total = 0
+    start = os.clock()
     for i = 1, n do
-        start = os.clock()
         obj4 = obj3 "obj4" { four = 4, four4 = 4, four44 = 4 }
-        stop = os.clock()
-        total = total + stop - start
     end
+    stop = os.clock()
     print("3rd level, name = 'obj4', 3 objects per level")
-    print("  Average speed: "..tostring(total/n))
+    print("  Average speed: "..tostring((stop-start)*1000000/n).."us")
 end
 
 ----------------------------------------------------------------------
