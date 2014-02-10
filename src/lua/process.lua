5e9f3a2788f96b4a56e12364638c065fc2c3855f
martinvalen <martin.valen1@gmail.com>
2014-02-10 11:21:58 +0100

    Added better documentation

--- src/lua/process.lua
+++ src/lua/process.lua
@@ -1,12 +1,54 @@
 local M = { help = {}, test = {} }
 
-M.help.self = [[
+M.help.self = [==[
 NAME
   lua.process
   
 SYNOPSIS
+  local pr    = require 'lua.process'
+  local probj = pr.open( "NAME cmdlnarg", 'r'/'w')
+  probj:write "String to write\n"
+  probj:flush()
+  probj:lines(...) -- ... = format
+  probj:read(...)  -- ... = format
+  probj:seek([whence [,offset]])
+  probj:setvbuf(mode[,size])
+  probj:close()
   
-]]
+DESCRIPTION
+  pr.open
+    Opens NAME with command line arguments cmdlnarg in read or write mode (default: 'r').
+    Returns a table containing the process and process' functions.
+  probj:write(str)
+    Writes str to the process, flushes with \n.
+  probj:flush()
+    Saves the data written to the process.
+  probj:lines(...)
+    Returns an iterator function that, each time it is called, reads the process
+    according to the given formats (default: '*l').
+  probj:read(...)
+    Reads the process, according to the given formats, which specifies what to read.
+    Formats:
+      '*n': reads a number and returns it (returned as number, not string).
+      '*a': reads all, starts from current position.
+      '*l': reads the next line, skipping '\n'. Default format.
+      number: reads number bytes and returns the string it has read.
+  probj:seek([whenche[,offset]])
+    Sets and gets the file postion, from start of file, to offset+base.
+    base is set by whence, which can be 'set' (base is pos 0), 'cur' (base is
+    current pos), or 'end' (base is end of file). whenche = cur and offset = 0 is default.
+  probj:setvbuf(mode[,size])
+    Sets the buffering mode for an output file. mode can be 'no' buffering, 'full',
+    or 'line' buffering. Size specifies the buff_size.
+  probj:close()
+    Closes the process.
+
+RETURN VALUES
+  A table containing pr.open.
+
+SEE ALSO
+  http://www.lua.org/manual/5.2/manual.html#pdf-io.popen
+]==]
 
 local function lines(self,...)
     return self.process:lines(...)
@@ -55,7 +97,7 @@ end
 M.help.gnuplot = [[
 Example usage for gnuplot:
 
-local pr = require'lua.process'()
+local pr = require'lua.process'
 local gp = pr.open('gnuplot', 'w')
 -- To make x11 window stay open after closing gnuplot.
 -- local gp = pr.open('gnuplot -persist', 'w')
