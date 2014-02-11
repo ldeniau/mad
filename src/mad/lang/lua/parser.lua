2b5497a1a51839efed4ded27b9d8ae7a9e959627
martinvalen <martin.valen1@gmail.com>
2014-02-11 14:00:20 +0100

    Prettify

--- src/mad/lang/lua/parser.lua
+++ src/mad/lang/lua/parser.lua
@@ -26,7 +26,7 @@ SEE ALSO
 local re      = require"lib.lpeg.re"
 local grammar = require"mad.lang.lua.grammar".grammar
 local defs    = require"mad.lang.lua.defs".defs
-local utest   = require"mad.core.unitTest"
+local utest   = require"mad.tester"
 
 
 -- metamethods ----------------------------------------------------------------
@@ -39,23 +39,9 @@ end
 
 -- module ---------------------------------------------------------------------
 
---[[local function countNodes(table)
-    local ret = 0
-    for k,v in pairs(table) do
-        if type(v) == "table" then
-            ret = ret+1+countNodes(v)
-        end
-    end
-    return ret
-end]]
-
 local parse = function (self, str, fileName, pos, line)
 	defs._line = line
-	local startTime = os.clock()
 	local ast = self.grammar:match(str, position)
-	local totalTime = os.clock() - startTime
-    --print(string.format("elapsed time: %.2fs", totalTime))
-    --print("number of nodes:             "..tostring(countNodes(ast)))
 	return ast
 end
 
