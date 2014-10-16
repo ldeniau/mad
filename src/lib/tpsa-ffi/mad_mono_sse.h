1c2198f3ea448dfa2027a3dbf9a901edbc610c8f
Laurent Deniau <laurent.deniau@gmail.com>
2014-10-16 20:52:57 +0200

    better SSE2

--- src/lib/tpsa-ffi/mad_mono_sse.h
+++ src/lib/tpsa-ffi/mad_mono_sse.h
@@ -22,29 +22,38 @@ mono_add_sse(int n, const ord_t a[n], const ord_t b[n], ord_t r[n])
 {
   assert(a && b && r);
   __m128i ra, rb, rr;
-  int i;
+  int i=0;
 
-  for (i=0; i < SSE_CRND(n); i+=SSE_CSIZ) {
+  for (; i < SSE_CRND(n); i+=SSE_CSIZ) {
     ra = _mm_loadu_si128((__m128i*)&a[i]);
     rb = _mm_loadu_si128((__m128i*)&b[i]);
     rr = _mm_adds_epi8(ra,rb);
     _mm_storeu_si128((__m128i*)&r[i],rr);
   }
 
-  for (int j=0; j < SSE_CMOD(n); j++)
-    r[i+j] = a[i+j] + b[i+j];
+  if (SSE_CMOD(n)) {
+    __m128i _0xFF = _mm_cmpeq_epi8(ra,ra);
+    __m128i mask  = _mm_srli_si128(_0xFF, SEE_CSIZ-SSE_CMOD(n));
+    ra = _mm_loadu_si128((__m128i*)&a[i]);
+    rb = _mm_loadu_si128((__m128i*)&b[i]);
+    rr = _mm_adds_epi8(ra,rb);
+    _mm_maskmoveu_si128(rr, mask, &r[i]);
+  }
+
+//  for (int j=0; j < SSE_CMOD(n); j++)
+//    r[i+j] = a[i+j] + b[i+j];
 }
 
 static inline int
 mono_sum_sse(int n, const ord_t a[n])
 {
   assert(a);
-  __m128i zero = _mm_setzero_si128();
+  __m128i _0x00 = _mm_setzero_si128();
   __m128i ra;
   int i, s=0;
 
   for (i=0; i < SSE_CRND(n); i+=SSE_CSIZ) {
-    ra = _mm_sad_epu8(_mm_loadu_si128((__m128i*)&a[i]), zero);
+    ra = _mm_sad_epu8(_mm_loadu_si128((__m128i*)&a[i]), _0x00);
     s += _mm_cvtsi128_si32(_mm_srli_si128(ra,8)) + _mm_cvtsi128_si32(ra);
   }
 
