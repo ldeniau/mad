8c13b8b5f252f139f0f338493737e7999e32b16d
Ciprian Tomoiaga <ciprian.tomoiaga@gmail.com>
2015-03-26 17:35:00 +0100

    added make_mul_input for bench_mul

---
+++ src/make_mul_input.lua
@@ -0,0 +1,19 @@
+local tpsa, factory = require "lib.tpsa", require "factory"
+
+tpsa.set_package("ffi")
+
+local nv, no = tonumber(arg[1]), tonumber(arg[2])
+
+local t = tpsa.init(nv,no)
+for i=0,nv do
+  t:set_at(i,1.2)
+end
+
+local a = t:cpy()
+for o=1,no do
+  a = a * t
+end
+
+local file = io.open(arg[3] or "tpsa.out","w")
+a:print(file)
+a:print(file)
