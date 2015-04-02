7504182f299ea71617b09b7a3a64477a48b05aa9
Ciprian Tomoiaga <ciprian.tomoiaga@gmail.com>
2015-04-02 14:21:38 +0200

    added timers to bench_mul; make_mul_input with random

--- src/make_mul_input.lua
+++ src/make_mul_input.lua
@@ -1,19 +1,20 @@
 local tpsa, factory = require "lib.tpsa", require "factory"
 
+io.stderr:write([[Usage: luajit make_mul_input.lua nv no [output_file]
+  output_file defaults to ./tpsa.out
+]])
+
 tpsa.set_package("ffi")
 
 local nv, no = tonumber(arg[1]), tonumber(arg[2])
 
 local t = tpsa.init(nv,no)
-for i=0,nv do
-  t:set_at(i,1.2)
-end
+t:rand(1.1, 5.5, os.time())
 
-local a = t:cpy()
-for o=1,no do
-  a = a * t
-end
+local filename = arg[3] or "tpsa.out"
+local file = io.open(filename,"w")
+t:print(file)
+t:print(file)
 
-local file = io.open(arg[3] or "tpsa.out","w")
-a:print(file)
-a:print(file)
+io.stderr:write(string.format("Done! Written '%s' for nv=%d no=%d\n", filename, nv, no))
+
