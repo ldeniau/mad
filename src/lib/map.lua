f558423e77268926e40797ece45829ca0dc345f5
Laurent Deniau <laurent.deniau@gmail.com>
2015-05-22 15:01:30 +0200

    private vars for maps

--- src/lib/map.lua
+++ src/lib/map.lua
@@ -1,22 +1,38 @@
 local tpsa = require"lib.tpsaFFI"
 
 local M = {}  -- this module
-local MT = { __index=M }
+local V = {}  -- private keys
+local D = {}  -- private desc
+local MT = {  -- metatable
+  __index = function (tbl, key)
+    return tbl[V][key] or M[key]
+  end,
+
+  __newindex = function (tbl, key, val)
+    local var = assert(tbl[V][key], "invalid map variable")
+    if type(var) == "number" then
+      tbl[V][key] = type(val) == "number" and val or val.coef[0]
+    elseif type(val) == "number" then
+      tpsa.setConst(var, val)
+    else
+      tpsa.cpy(val, var)
+    end 
+  end
+}
 
 -- {v={'x','px'}, mo={2,2} [, vo={3,3}] [, ko={1,1}] [, dk=2]}
-function M.make_map(args)
+function M:make_map(args)
   assert(args and args.v and args.mo and #args.v == #args.mo)
-  local m = {}
+  local m = { [V]={} }
   args.vo = args.vo or args.mo
-  m._desc = tpsa.get_desc(args)
+  m[D] = tpsa.get_desc(args)
   for i=1,#args.mo do
     if args.mo[i] == 0 then
-      m[args.v[i]] = 0
+      m[V][args.v[i]] = 0
     else
-      m[args.v[i]] = tpsa.allocate(m._desc,args.mo[i])
+      m[V][args.v[i]] = tpsa.allocate(m[D],args.mo[i])
     end
   end
-
   return setmetatable(m,MT)
 end
 
@@ -27,7 +43,7 @@ function M:to(...)
     to = mo > to and mo or to
   end
 
-  tpsa.gtrunc(self._desc,to)
+  tpsa.gtrunc(self[D],to)
 end
 
 
